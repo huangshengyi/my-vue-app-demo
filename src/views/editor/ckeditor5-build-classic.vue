51518caa2c2be4047843e91e337b4a6a6d53688d
hsy <2458767808@qq.com>
2021-01-31 17:02:32 +0800

    ckeditor5-build-classic经典编辑富文本编辑器引入

--- src/views/editor/ckeditor5-build-classic.vue
+++ src/views/editor/ckeditor5-build-classic.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
+  <div class="ckeditor5-build-classic-wrapper">
     ckeditor5-build-classic经典编辑富文本编辑器
+    <div id="editor" ref="editor">
+      <p>Here goes the initial content of the editor.</p>
+    </div>
   </div>
 </template>
+
+<script>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
+export default {
+  mounted() {
+    this.initEditor()
+  },
+  methods: {
+    initEditor() {
+      ClassicEditor
+      .create(this.$refs.editor)
+      .then(editor => {
+          console.log(editor)
+      })
+      .catch(error => {
+          console.error(error)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ckeditor5-build-classic-wrapper {
+  background: #eee;
+}
+</style>
